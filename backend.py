d457bb0f3839a0116ae70859c2454ce02031f25e
alvinc <alvincUCLA@gmail.com>
2024-02-12 15:49:52 -0800

    backend and data from weather website added

---
+++ backend.py
@@ -0,0 +1,11 @@
+import requests
+
+API_KEY = "893bfdd4e3a301aeab064b5253d03cc7"
+def get_data(place, forecast_days=None, kind=None):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+if __name__=="__main__":
+    print(get_data(place="Tokyo"))
